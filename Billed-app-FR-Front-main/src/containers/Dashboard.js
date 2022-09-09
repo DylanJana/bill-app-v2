ae26d0e2aecad616c18b94d38cd700778154dc50
DylanJana <dylan@janaconcierge.com>
2022-09-09 11:56:40 +0200

    Correction d'un bug supplémentaire sur l'ouverture des onglets parents

--- Billed-app-FR-Front-main/src/containers/Dashboard.js
+++ Billed-app-FR-Front-main/src/containers/Dashboard.js
@@ -137,13 +137,17 @@ export default class {
     e.preventDefault();
       if (this.counter === undefined || this.index !== index) this.counter = 0
       if (this.index === undefined || this.index !== index) this.index = index
-      if (this.counter % 2 === 0) {
+      let arrowObjectIcon = $(`#arrow-icon${this.index}`);
+      let arrowIcon = arrowObjectIcon[0];
+      if (this.counter % 2 === 0 && !arrowIcon.classList.contains('opened')) {
         $(`#arrow-icon${this.index}`).css({ transform: 'rotate(0deg)'})
+        arrowIcon.classList.add('opened')
         $(`#status-bills-container${this.index}`)
           .html(cards(filteredBills(bills, getStatus(this.index))))
         this.counter ++
       } else {
         $(`#arrow-icon${this.index}`).css({ transform: 'rotate(90deg)'})
+        arrowIcon.classList.remove('opened')
         $(`#status-bills-container${this.index}`)
           .html("")
         this.counter ++
