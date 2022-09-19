5173f766bd02e08e79498086eebe40fc8c5d61d0
DylanJana <dylan@janaconcierge.com>
2022-09-19 22:32:49 +0200

    Fin test Bills.js

--- Billed-app-FR-Front-main/src/__tests__/NewBill.js
+++ Billed-app-FR-Front-main/src/__tests__/NewBill.js
@@ -5,19 +5,28 @@
 import { fireEvent, screen } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js";
-import {mockStore, bills} from "../__mocks__/store.js"; // Ajout
+import {mockStore} from "../__mocks__/store.js"; // Ajout
 import {localStorageMock} from "../__mocks__/localStorage.js";
 import {ROUTES} from "../constants/routes.js";
 
 describe("Given I am connected as an employee", () => {
+  let newBill;
+  beforeEach(() => {
+    Object.defineProperty(window, "localStorage", { value: localStorageMock });
+    window.localStorage.setItem("user", JSON.stringify({ type: "Employee" }));
+    const html = NewBillUI();
+    document.body.innerHTML = html;
+    newBill = new NewBill({
+      document,
+      onNavigate: (pathname) =>
+        (document.body.innerHTML = ROUTES({ pathname })),
+      mockStore: mockStore,
+      localStorage: window.localStorage,
+    });
+  });
+
   describe("When I am on NewBill Page", () => {
     test("Then element with text Envoyer une note de frais is available", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
-      window.localStorage.setItem('user', JSON.stringify({
-        type: 'Employee'
-      }))
       expect(screen.getByText('Envoyer une note de frais')).toBeTruthy();
       expect(screen.getByTestId('form-new-bill')).toBeTruthy();
       //to-do write assertion
@@ -33,30 +42,18 @@ describe("Given I am connected as an employee", () => {
     })
   })
 
-  describe('When I am on NewBill Page', () => {
-    test('I click to button to change file', async() => {
+  describe('When I select a file', () => {
+    test('It should call handleChangeFile method', async() => {
 
-      let buttonChangeFile = screen.getByTestId('file');
-      const onNavigate = (pathname) => {
-        document.body.innerHTML = ROUTES({ pathname });
-      };
-      const newBill = new NewBill({
-        // Envoi d'une note de frais
-        document,
-        onNavigate,
-        mockStore,
-        localStorage: window.localStorage,
+      const handleChangeFile = jest.fn(newBill.handleChangeFile);
+      const inputFile = screen.getByTestId("file");
+      inputFile.addEventListener("change", handleChangeFile);
+      fireEvent.change(inputFile, {
+        target: {
+          files: [new File(["proof.jpg"], "proof.jpg", { type: "image/jpg" })],
+        },
       });
-
-      const handleChangeFile = jest.fn((e) =>
-        newBill.handleChangeFile(e),
-      );
-
-      buttonChangeFile.addEventListener("change", handleChangeFile); // écoute du changement
-      fireEvent.change(buttonChangeFile); // Event change virtuel sur le bouton "Parcourir"
-      expect(handleChangeFile).toHaveBeenCalled(); // Vérifie que la fonction simulée est appelée
-      //expect(screen.getByText("Envoyer une note de frais")).toBeTruthy(); // Vérifie que le titre de la page est présent.
-      //expect(screen.getByTestId("form-new-bill")).toBeTruthy(); // Vérifie que le formulaire s'affiche
+      expect(handleChangeFile).toHaveBeenCalled();
     })
   })
 })
